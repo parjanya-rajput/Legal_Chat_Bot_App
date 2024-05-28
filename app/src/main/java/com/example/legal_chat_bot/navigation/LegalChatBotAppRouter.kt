8fba09a97e5803eb968823dd6e8f12bbba36a876
Parjanya Rajput <pajuraj05@gmail.com>
2024-05-28 16:37:24 +0530

    API Integration-1

--- app/src/main/java/com/example/legal_chat_bot/navigation/LegalChatBotAppRouter.kt
+++ app/src/main/java/com/example/legal_chat_bot/navigation/LegalChatBotAppRouter.kt
@@ -2,8 +2,6 @@ package com.example.legal_chat_bot.navigation
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.legal_chat_bot.data.LoginViewModel
 
 sealed class Screen(){
     object RegisterScreen : Screen()
